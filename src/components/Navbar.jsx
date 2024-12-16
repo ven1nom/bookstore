14ef3a563b7ceedf3b8e91bdfb58c71d1cf4abdc
ven1nom <your-email@example.com>
2024-12-16 21:54:06 +0530

    "restructure - login-redux-store.js-cartSlice"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,75 +1,108 @@
-import React from 'react';
-import { HiMiniBars3CenterLeft } from "react-icons/hi2";
-import { FcSearch } from "react-icons/fc";
-import { Link } from 'react-router';
-import { FaRegUser } from "react-icons/fa6";
-import { FaRegHeart } from "react-icons/fa";
-import { PiShoppingCartLight } from "react-icons/pi";
-import avatarImg from "../assets/avatar.png"
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { HiMiniBars3CenterLeft, HiOutlineHeart, HiOutlineShoppingCart } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
 
-//dropdown menu
-const navigation=[
-  {name:'Dashboard' , href:'/dashboard'},
-  {name:'Orders' , href:'/order'},
-  {name:'Cart Page',href:'/cart'},
-  {name:'Dashboard',href:'/checkout'},
+import avatarImg from "../assets/avatar.png"
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useAuth } from "../context/AuthContext";
+
+const navigation = [
+    {name: "Dashboard", href:"/user-dashboard"},
+    {name: "Orders", href:"/orders"},
+    {name: "Cart Page", href:"/cart"},
+    {name: "Check Out", href:"/checkout"},
 ]
 
-//conditional rendering of user
-let currentUser=false;
 const Navbar = () => {
 
-//dropdown menu
-const [isDropDown,setIsDropDown]=useState(false);
+    const  [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    //cart items -> react redux
+    const cartItems = useSelector(state => state.cart.cartItems);
+   
+    const {currentUser, logout} = useAuth()
+    
+    const handleLogOut = () => {
+        logout()
+    }
 
- return (
-<header className=' py-8 px-10 border-2 border-black-500'>
-    <nav className='flex justify-between border-2 border-sky-500'>
-        {/*left side */}
-        <div className='flex items-center '>
-            <Link to=""><HiMiniBars3CenterLeft className='mr-10'/> </Link>
-           <div className='flex bg-slate-200 p-2 rounded-md'>
-           <FcSearch className='mt-1  mr-2'/>
-           <input type="text" placeholder="What are you looking for" className='border-none focus:outline-none bg-slate-200 pr-20'/>
-           </div>
-        </div>
-        {/*right side */}
-        <div className='flex items-center relative'>
-        <div>
-            {currentUser ? <>
-                <button onClick={()=>setIsDropDown(!isDropDown)}>
-                    <img src={avatarImg} alt="avatar_image" className='relative mr-5'/>
-                </button>         
-                {/*Show Dropdowns    */}
-            {isDropDown && (
-              <div className='absolute mt-1 '>
-                <ul className='pr-10 bg-slate-100 z-10 '>
-                  {
-                    navigation.map((item)=>(
-                      <li key={item.name} className='hover:bg-slate-300  pl-2'>
-                        <Link to={item.href}>
-                        {item.name}
-                        </Link>
-                      </li>
-                    ))
-                  }
-                </ul>
-              </div>
-            )}</>: (
-               <Link to="/login"><FaRegUser className='mr-5' /></Link> 
-            )}
-        </div>
-           <button><FaRegHeart className='mr-5'/></button>
-           <div className='flex items-center bg-yellow-300 py-2 px-2 rounded-xl'>
-            <Link to="/cart"> <PiShoppingCartLight className='ml-3 mr-3' />
-            </Link>
-            <span className='ml-3 mr-8'>Basket</span>
-           </div> 
-        </div>
-    </nav>
-</header>
-  )
+    const token = localStorage.getItem('token');
+  
+    return (
+        <header className="max-w-screen-2xl mx-auto px-4 py-6">
+            <nav className="flex justify-between items-center">
+                {/* left side */}
+                <div className="flex items-center md:gap-16 gap-4">
+                    <Link to="/">
+                        <HiMiniBars3CenterLeft className="size-6" />
+                    </Link>
+
+                    {/* search input */}
+                    <div className="relative sm:w-72 w-40 space-x-2">
+
+                        <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+
+                        <input type="text" placeholder="Search here"
+                            className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+                        />
+                    </div>
+                </div>
+
+
+                {/* rigth side */}
+                <div className="relative flex items-center md:space-x-3 space-x-2">
+                    <div >
+                        {
+                            currentUser ? <>
+                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                                <img src={avatarImg} alt="" className={`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500' : ''}`} />
+                            </button>
+                            {/* show dropdowns */}
+                            {
+                                isDropdownOpen && (
+                                    <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                                        <ul className="py-2">
+                                            {
+                                                navigation.map((item) => (
+                                                    <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
+                                                        <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
+                                                            {item.name}
+                                                        </Link>
+                                                    </li>
+                                                ))
+                                            }
+                                            <li>
+                                                <button
+                                                onClick={handleLogOut}
+                                                className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100">Logout</button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                )
+                            }
+                            </> : token ?  <Link to="/dashboard" className='border-b-2 border-primary'>Dashboard</Link> : (
+                                <Link to="/login"> <HiOutlineUser className="size-6" /></Link>
+                            )
+                        }
+                    </div>
+                    
+                    <button className="hidden sm:block">
+                        <HiOutlineHeart className="size-6" />
+                    </button>
+
+                    <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm">
+                        <HiOutlineShoppingCart className='' />
+                        {
+                            cartItems.length > 0 ?  <span className="text-sm font-semibold sm:ml-1">{cartItems.length}</span> :  <span className="text-sm font-semibold sm:ml-1">0</span>
+                        }
+                        
+                       
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
-export default Navbar
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router'
 import router from './router/router.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router}/>
-  </StrictMode>,
+  </Provider>,
 )
